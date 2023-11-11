845191a832b1e355ea0f6a2766f297ae23ab6543
AnwarMelhem <anwarmelhem2000@gmail.com>
2023-11-11 09:22:22 +0200

    Setup Project

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
    
-    specPattern: "cypress/e2e/*.feature",
+    specPattern: "cypress/e2e/**/*.feature",
     async setupNodeEvents(on, config) {
       // implement node event listeners here
       return require("./cypress/plugins")(on, config);
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,9 +1,8 @@
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const addCucumberPreprocessorPlugin =
-require("@badeball/cypress-cucumberpreprocessor").
-addCucumberPreprocessorPlugin;
+require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 const createEsBuildPlugin =
-require("@badeball/cypress-cucumberpreprocessor/esbuild").createEsbuildPlugin;
+require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 module.exports = async (on, config) => {
 const bundler = createBundler({
 plugins: [createEsBuildPlugin(config)],
