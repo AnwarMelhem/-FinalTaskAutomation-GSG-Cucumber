dbeb0af11e160e72813c47cf70122e5b64fc369d
AnwarMelhem <anwarmelhem2000@gmail.com>
2023-11-11 13:09:27 +0200

    add generate random email function

--- cypress/support/helpers/generic-helper.ts
+++ cypress/support/helpers/generic-helper.ts
@@ -12,4 +12,29 @@ export default class GenericHelper {
     const currentDate = `${year}-${month}-${day}`;
     return currentDate;
   }
+
+  static generateRandomEmail() {
+    // Define a function to generate a random string with a given length
+    function generateRandomString(length:any) {
+      const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-';
+      let result = '';
+      for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+      }
+      return result;
+    }
+    // Generate a random username with a random length between 5 and 10 characters
+    const usernameLength = Math.floor(Math.random() * (10 - 5 + 1) + 5);
+    const username = generateRandomString(usernameLength);
+    // Generate a random domain with a random length between 5 and 10 characters
+    const domainLength = Math.floor(Math.random() * (10 - 5 + 1) + 5);
+    const domain = generateRandomString(domainLength);
+    // Choose a random top-level domain (TLD) from a predefined list
+    const tlds = ["com", "net"];
+    const tld = tlds[Math.floor(Math.random() * tlds.length)];
+    // Concatenate the username, domain, and TLD to form the email address
+    const email = `${username}@${domain}.${tld}`;
+    return email;
+  }
+  
 }
